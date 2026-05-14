5b39654c052126e188301f9e0c8461194e08e303
automa026-lab <automa026@gmail.com>
2026-05-14 09:22:08 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from './App';
-
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
